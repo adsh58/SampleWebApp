b60533ebfb37d03adf17586d4c12f1ac68040616
Adarsh Vishwakamra <vamadarsh58@outlook.com>
2025-12-26 10:31:32 +0530

    Initial commit: Employee Management System with SQL Server

---
+++ SampleConsoleApp/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    public class AppDbContext : DbContext
+    {
+        //public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        //{
+
+        //}
+        public AppDbContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=SchoolDb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            try
+            {
+                var employees = await Employees
+                    .FromSqlRaw("SELECT * FROM Employee")
+                    .ToListAsync();
+                return employees;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+
+
+    public class Employee
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Skill { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleConsoleApp;
+using System;
+
+
+Console.WriteLine("Start the application! \n");
+
+using (var context = new AppDbContext())
+{
+    await context.Database.EnsureCreatedAsync();
+
+    var empList = await context.GetAllEmployeesAsync();
+    Console.WriteLine("Users from db.\n");
+    foreach (var emp in empList)
+    {
+        Console.WriteLine($"EmpId : {emp.EmpId} Name : {emp.Name} Skill : {emp.Skill}\n");
+    }
+    Console.WriteLine("End\n");
+}
